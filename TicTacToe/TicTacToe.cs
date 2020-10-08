a19bae698928cf997d4c9e6306fdc41410b692e4
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-08 10:19:19 +0530

    [Rachit] Add. Initialised char Array

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -8,7 +8,7 @@ namespace TicTacToe
     {
         private char[] boardArray = new char[10];
         
-        public void UC_1()//Create Game by initialising char array starting with poistion 1
+        public void UC_1()//Create Game by initialising char array 
         {
          
             for(int position=1; position < boardArray.Length; position++)
@@ -17,26 +17,6 @@ namespace TicTacToe
             }
 
         }
-        public void UC_2()//
-        {
-            Console.WriteLine("Select X or 0");
-            char userSelection = Convert.ToChar(Console.ReadLine());
-            char compSelection = ' ';
-
-            if (userSelection == 'X')
-            {
-                compSelection = '0';
-            }
-            else if (userSelection == '0')
-            {
-                compSelection = 'X';
-            }
-            else
-            {
-                Console.WriteLine("Please select correctly");
-                userSelection = '-';
-                compSelection = '-';
-            }
-        }
+        
     }
 }
