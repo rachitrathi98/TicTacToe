870a19bfa451c8fa28064db434f3790fe6744995
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-08 10:04:27 +0530

    [Rachit] Add. Initialised char Array

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -6,6 +6,17 @@ namespace TicTacToe
 {
     class TicTacToe
     {
+        private char[] boardArray = new char[10];
+        
+        public void UC1()
+        {
+            Console.WriteLine("UC1");
+            for(int position=1; position < boardArray.Length; position++)
+            {
+                Console.WriteLine("Hi");
+                boardArray[position] = ' ';
+            }
 
+        }
     }
 }
